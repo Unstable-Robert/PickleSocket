5769c3fad5c70ac32350bcbf4cc9859058ca954b
Robert Mannuzza <Robert.mannuzza@gmail.com>
2023-10-12 19:23:48 -0400

    Cleaned up imports hopefully

--- PickleSocket/__init__.py
+++ PickleSocket/__init__.py
@@ -1,5 +1,4 @@
-from PickleUtility import Utility, SocketStatus
-from PickleMessage import Message, MessageType
+from PickleSocket.PickleUtility import Utility, SocketStatus
+from PickleSocket.PickleMessage import Message, MessageType
 from PickleSocket.PickleSocket import PickleSocket
-
 
