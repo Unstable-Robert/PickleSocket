bd740518118ac2cc40dd339eba3facb7e0746626
Robert Mannuzza <Robert.mannuzza@gmail.com>
2023-12-08 19:41:20 -0500

    updated init

--- PickleSocket/__init__.py
+++ PickleSocket/__init__.py
@@ -1,4 +1,4 @@
-from PickleSocket.PickleUtility import Utility, SocketStatus
+from PickleSocket.PickleUtility import Utility, SocketStatus, Protocol
 from PickleSocket.PickleMessage import Message, MessageType
 from PickleSocket.PickleSocket import PickleSocket
 
